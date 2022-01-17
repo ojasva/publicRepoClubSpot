8c0b120ac70806da3e6aa1134fba8d1454c5a943
anjaliram7 <69080715+anjaliram7@users.noreply.github.com>
2022-01-17 22:19:37 +0530

    Added frontend files

---
+++ src/Components/Pages/Club/Club.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import "./Clubs.css"
+
+function Club() {
+    return (
+        <div>
+            <div className="vit">
+            <h2 className='add'>VIT-AP University</h2>
+            </div>
+            <div class="parent-parallel">
+                <div class="parallel-1">
+                <p>Technical clubs</p>   
+                <div class="columns">
+                <div class="column-1">
+          
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                </div>
+                <div class="column-2">
+          
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                </div>
+                </div>
+                </div>
+               
+          
+                <div class="parallel-2">
+                <p>Non-technical clubs</p>
+                <div class="column-3">
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                </div>
+                <div class="column-4">
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                <div className="club-box">
+                <a href ='#'>
+                  <img src="https://images.unsplash.com/photo-1638317004711-62044568893a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"></img>
+                  <p>Club</p>
+                  </a>
+                </div>
+                </div>
+                </div>
+            </div>
+            </div>
+       
+        
+        
+    )   
+}
+export default Club;
